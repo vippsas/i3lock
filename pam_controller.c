681b896438168bec5607035adff295bd4afbb7d9
Claudio Bartoli <claudio.bartoli@vipps.no>
2026-08-27 10:07:57 +0200

    Do not reject login when pam_setcred() fails

--- pam_controller.c
+++ pam_controller.c
@@ -444,8 +444,11 @@ static void *worker_main(void *arg) {
         }
 
         if (ret == PAM_SUCCESS && !cancellation_requested_locked_read()) {
-            ret = pam_setcred(handle, PAM_REFRESH_CRED);
-            terminal_status = ret;
+            int setcred_ret = pam_setcred(handle, PAM_REFRESH_CRED);
+            if (setcred_ret != PAM_SUCCESS) {
+                fprintf(stderr, "[i3lock] PAM worker: pam_setcred(PAM_REFRESH_CRED) failed (%d)\n", setcred_ret);
+                terminal_status = setcred_ret;
+            }
         }
 
         if (cancellation_requested_locked_read()) {
